ddde8179cd9f910414044b6746db9f32eda73f84
yuzujr <15568103056@163.com>
2025-05-02 10:23:09 +0000

    将config.json加入ci/cd脚本

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,2 +1,3 @@
 cmake -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -S . -B build
 cmake --build build --config Release --target ScreenUploader
+cp /root/ScreenUploader/config.json build
